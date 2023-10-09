cc384aad01a28bdc3ae85f70006e31622c818e5b
Douglas Martin <mdgeek1@gmail.com>
2023-10-09 14:16:50 -0400

    Add post build validation.

---
+++ validate_.sh
@@ -0,0 +1,7 @@
+file_exists "import-map.json"
+file_exists "README.md"
+file_contains "LICENSE" "Copyright (c) JS Foundation"
+
+file_exists "dist/locales.js"
+file_exists "dist/moment.js"
+file_exists "dist/moment-with-locales.js"
