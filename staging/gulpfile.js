11cd5c1ff7e79c45129f0121c93c992c907f67d8
Douglas Martin <mdgeek1@gmail.com>
2023-10-14 10:14:44 -0400

    POM refactoring.

--- staging/gulpfile.js
+++ staging/gulpfile.js
@@ -1,6 +1,6 @@
 const {src, dest, series} = require('gulp');
 
-const srcDir = '${src.rootdir}/node_modules/moment/';
+const srcDir = '${webjar.staging}/node_modules/moment/';
 const destDir = '${webjar.target}/';
 
 function task1() {
